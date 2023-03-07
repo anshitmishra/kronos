8a02cf67e72d83b7c74449fe49cd32042bb42920
anshitmishra <anshitmishra03@gmail.com>
2023-03-07 09:08:37 +0530

    add final test

--- src/page.tsx
+++ src/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from '../styles/page.module.css'
 import Header from './header'
 import AOS from "aos";
@@ -21,12 +20,11 @@ export default function Home() {
     <>
       <Header />
       <div className={styles.main} >
-        <div style={{height:"80px",background:"black"}}></div>
+        <div style={{ height: "80px", background: "black" }}></div>
         <div className={styles.mainHolder} >
-
           <div className={styles.mainHolderTwo} >
             <p id={styles.design1} data-aos="fade-up"
-     data-aos-duration="3000"><img src="./design1.svg" alt="design one" /></p>
+              data-aos-duration="3000"><img src="./design1.svg" alt="design one" /></p>
             <p id={styles.design2} data-aos="fade-left" data-aos-duration="3000"><img src="./design2.svg" alt="design one" /></p>
             <div className={styles.mainHolderTwoHolder} >
               <h2 className={styles.mainHolderTwoHeading}>augmented reality</h2>
@@ -35,87 +33,71 @@ export default function Home() {
               <h4>2023 biggest Tech Event Join us & Make the AR filter with us.</h4>
               <br />
               <button ><b>Register Now <BsArrowRight style={{ margin: "0px 5px" }} /></b></button>
+              <br />
+              <h4>amazing prizepoll and goodies</h4>
+
             </div>
           </div>
           <div className={styles.mainHolderOne} >
-            <p id={styles.design3}  data-aos="zoom-in" data-aos-duration="3000"><img src="./design3.svg" alt="design one" /></p>
-
-            <div className={styles.mainHolderOneMainImage}  data-aos="zoom-in">
+            <p id={styles.design3} data-aos="zoom-in" data-aos-duration="3000"><img src="./design3.svg" alt="design one" /></p>
+            <div className={styles.mainHolderOneMainImage} data-aos="zoom-in">
               <img src={"/item1.png"} alt='logo' style={{ objectFit: "cover" }} ></img>
             </div>
           </div>
         </div>
-
-
         {/*  */}
-
-
         <div className={styles.mainHolder2}>
           <div className={styles.mainHolder2IOne}>
             <h2>Event Rules</h2>
             <br />
             <ul>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
-              <li data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus voluptate velit exercitationem labore ullam praesentium nihil ratione laboriosam id quod magnam, ipsum ad corporis ab est tenetur vel voluptates?</li>
+              <li data-aos="fade-right" data-aos-duration="1000">Bring your own laptop we will only provide internet connection in event</li>
+              <li data-aos="fade-right" data-aos-duration="1000">If anyone violates events rules they will be disqualified. </li>
+              <li data-aos="fade-right" data-aos-duration="1000">If participants are found doing any kind of cheating they will be disqualified.</li>
+              <li data-aos="fade-right" data-aos-duration="1000">Judgement of the respected jury is final.</li>
+              <li data-aos="fade-right" data-aos-duration="1000">If participants misbehave with the event team they will be disqualified immediately.</li>
+              <li data-aos="fade-right" data-aos-duration="1000">Use of any abusive word is restricted if participants use any abusive word they will be disqualified.</li>
+              <li data-aos="fade-right" data-aos-duration="1000">In the event, Registration ticket will require and in any case, you lost your ticket technical team will verify your identity</li>
             </ul>
-
           </div>
           <div className={styles.mainHolder2Two}>
             <h2>Event Details</h2>
             <br />
-            <p data-aos="fade-left" data-aos-duration="1000">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia a distinctio, id corporis assumenda omnis impedit, nisi hic eveniet maxime aut totam tempora sunt nemo nulla soluta? Rem, veritatis?
-              Tempore autem necessitatibus sunt hic error eum eveniet porro! Hic dignissimos odit rerum, minus commodi pariatur libero! Harum, ipsam mollitia suscipit cupiditate ad nisi assumenda totam! Odio aperiam distinctio soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, earum molestias dolorum cumque perferendis ipsam cum quasi enim obcaecati aspernatur aliquid dolore non dolorem numquam impedit, asperiores modi eum accusantium? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos, officiis earum? At quae molestias aliquid ipsum dolores totam repellendus, explicabo cum natus, eius porro perferendis. Quidem beatae fugiat accusamus accusantium?</p>
-
+            <p data-aos="fade-left" data-aos-duration="1000">The augmented reality event is based on making filters for social media like Facebook and Instagram in this event first half hour we will provide a workshop for AR software. We will explain how to make a filter. And then the competition starts. Participants need to make filters within a certain time. And the best filter will win. and the winner filter will be used in photo point in the event.</p>
           </div>
         </div>
-
         {/*  */}
-
         <div className={styles.mainContact}>
           <h2>Contact us</h2>
           <div className={styles.mainContactHolder}>
-
             <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
               <div className={styles.mainContactCardImage}>
                 <img src="/pranjal.jpg" alt="anshit" />
               </div>
-
               <div className={styles.cardDetails}>
                 <p>Pranjal sharma</p>
                 <span>Tech Coordinator</span>
               </div>
-              <div className={styles.cardContact}>
-              <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
-                <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
-                <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
-              </div>
-            </div>
-
-
-
-
-            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
-              <div className={styles.mainContactCardImage}>
-                <img src="/anshit2.jpeg" alt="anshit" />
-              </div>
-
-              <div className={styles.cardDetails}>
-                <p>anshit mishra</p>
-                <span>Tech Coordinator</span>
-              </div>
-
               <div className={styles.cardContact}>
                 <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
                 <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
                 <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
               </div>
             </div>
-
-
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/anshit2.jpeg" alt="anshit" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>anshit mishra</p>
+                <span>Tech Coordinator</span>
+              </div>
+              <div className={styles.cardContact}>
+                <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
+                <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
+                <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
+              </div>
+            </div>
             <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
               <div className={styles.mainContactCardImage}>
                 <img src="/rahul.jpg" alt="rahul" />
@@ -124,14 +106,12 @@ export default function Home() {
                 <p>Rahul Bhadauriya</p>
                 <span>Tech Coordinator</span>
               </div>
-
               <div className={styles.cardContact}>
-              <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
+                <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
                 <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
                 <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
               </div>
             </div>
-
           </div>
         </div>
         <div className={styles.footer}>
